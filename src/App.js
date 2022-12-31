914c2d957ac3940f14636256213958f9bcc44390
Tosin-Kasaba <tosinkasaba1@gmail.com>
2022-12-31 00:17:15 -0700

    fix: error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar.jsx";
 import Explore from "./pages/Explore";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -21,6 +22,8 @@ function App() {
         </Routes>
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
