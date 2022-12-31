ad44ddae7e27d4833aa18f44e19fcea3819d474e
Tosin-Kasaba <tosinkasaba1@gmail.com>
2022-12-30 18:27:15 -0700

    feat:navbar components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import {useNavigate, useLocation} from 'react-router-dom';
+import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
+import {ReactComponent as ExploreIcon} from '../assets/svg/ExploreIcon.svg'
+import {ReactComponent as PersonOutlineIcon} from '../assets/svg/PersonOutlineIcon.svg'
+
+function Navbar() {
+  return (
+    <footer className = 'navbar'> 
+        <nav className='navbarBav'>
+            <ul className="navbarListItems">
+                <li className="navbarListItem">
+                 <ExploreIcon fill="#2c2c2c" width="36px" height="36px" />
+                 <p>Explore</p>
+                </li>
+                <li className="navbarListItem">
+                 <OfferIcon fill="#2c2c2c" width="36px" height="36px" />
+                 <p>Offer</p>
+                </li>
+                <li className="navbarListItem">
+                 <PersonOutlineIcon fill="#2c2c2c" width="36px" height="36px" />
+                 <p>Profile</p>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+  )
+}
+
+export default Navbar
