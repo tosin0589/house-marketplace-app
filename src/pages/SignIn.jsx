84eddda583b2b0803d4597068a6e03bad1582fd4
Tosin-Kasaba <tosinkasaba1@gmail.com>
2022-12-30 23:51:31 -0700

    feat:  profile signin

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
-import{Link, useNavigate} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -21,6 +22,22 @@ const onChange  = (e) => {
   }));
 };
 
+const onSubmit = async (e) => {
+  e.preventDefault()
+
+  try{
+    const auth = getAuth()
+
+    const userCredentials = await signInWithEmailAndPassword (auth, email, password)
+    if(userCredentials.user) {
+      navigate('/')
+  }
+} catch (error) {
+  console.log(error);
+}
+
+  }
+
     return (
       <>
           <div className='pageContainer'>
@@ -28,7 +45,7 @@ const onChange  = (e) => {
               <p className='pageHeader'>Welcome Back!</p>
             </header>
 
-              <form>
+              <form onSubmit ={onSubmit}>
                 <input type="email" 
                 className= "emailInput"
                 placeholder="Email" 
